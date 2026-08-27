e2ad6ac364ccab9b36040e65723e113ea87ea9d9
Aria <aria.and.idriss@gmail.com>
2026-08-27 12:37:20 +0200

    Update game lists in Listes.py
    Added 'Gacha Nox' to the list of games and updated the overall game list.

--- app/static/Listes.py
+++ app/static/Listes.py
@@ -5,5 +5,5 @@ colonnes_G = ["Id", "Nom", "Image"]
 colonnes_sqlG = ["CharacterId", "CharacterName", "CharacterImage"]
 
 list_games = ["Gacha Club", "Gacha Life 2", "Gacha Nebula v1.6", "Gacha Plus", "Gacha Luminal", 
-              "Gacha Realms", "Gacha Ultra", "Gacha Life", "Gachaverse", "Gacha Want"]
+              "Gacha Realms", "Gacha Ultra", "Gacha Life", "Gachaverse", "Gacha Want", "Gacha Nox"]
 liste_all = ["Gacha Games", "Gacha Life", "Gacha Club", "Gacha Life 2", "Gacha Nebula v1.6"]
