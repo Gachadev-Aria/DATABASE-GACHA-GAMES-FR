4c830b9f988941f2d71747a8a42534501c1312d8
Aria <aria.and.idriss@gmail.com>
2026-09-03 22:39:04 +0200

    Update game lists and column names in Listes.py
    Add Minimuse

--- app/static/Listes.py
+++ app/static/Listes.py
@@ -4,6 +4,7 @@ colonnes_sqlGG = ["CharacterId", "CharacterName", "Game", "CharacterImage"]
 colonnes_G = ["Id", "Nom", "Image"]
 colonnes_sqlG = ["CharacterId", "CharacterName", "CharacterImage"]
 
-list_games = ["Gacha Club", "Gacha Life 2", "Gacha Nebula v1.6", "Gacha Plus", "Gacha Luminal", 
-              "Gacha Realms", "Gacha Ultra", "Gacha Life", "Gachaverse", "Gacha Want", "Gacha Nox"]
-liste_all = ["Gacha Games", "Gacha Life", "Gacha Club", "Gacha Life 2", "Gacha Nebula v1.6"]
+games = ["Gacha Club", "Gacha Life 2", "Gacha Nebula", "Gacha Plus", "Gacha Luminal", 
+        "Gacha Realms", "Gacha Ultra", "Gacha Life", "Gachaverse", "Gacha Want", "Gacha Nox", 
+        "Minimuse"]
+liste_all = ["Gacha Games", "GL", "GC", "GL2", "GN", "MM"]
